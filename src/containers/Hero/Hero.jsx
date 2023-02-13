91ce5159f44a186f92d84eaca7e0ae5a811799d7
hafizmp <hafiznjh007@gmail.com>
2023-02-14 00:25:09 +0530

    Added Hero

--- src/containers/Hero/Hero.jsx
+++ src/containers/Hero/Hero.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import "./Hero.scss"
+import { images } from "../../constants";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='hero'>
+      <div className="hero__container">
+        <div className="content">
+          <h1>Want to be a successful trader ?</h1>
+          <span>Learn & trade together with india’s #1
+            Trusted by over 4500+ Traders
+          </span>
+          <button>Get Started</button>
+        </div>
+        <div className="bg">
+          <img src={images.heroImg} alt="" />
+        </div>
+      </div>
+        <img className='vector' src={images.lineVector} alt="lv" />
+    </div>
   )
 }
 
