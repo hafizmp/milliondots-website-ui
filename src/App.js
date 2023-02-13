9d8700b82710db2a2ff27beaaadc146ca2a13f5c
hafizmp <hafiznjh007@gmail.com>
2023-02-13 22:50:55 +0530

    Added Global Styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import { Hero, About, Services, Testimonials, Enroll, Contact } from "./containers"
+import {  Navbar, Footer } from "./components"
 
 function App() {
   return (
     <>
+    <Navbar />
+    {/* <Hero /> */}
+    {/* <About /> */}
+    {/* <Services /> */}
+    {/* <Testimonials /> */}
+    {/* <Enroll /> */}
+    {/* <Contact /> */}
+    {/* <Footer /> */}
     </>
   );
 }
