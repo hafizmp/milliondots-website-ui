26004963650cb10b123b5c7781b4fb43464e93b3
hafizmp <hafiznjh007@gmail.com>
2023-02-13 23:34:26 +0530

    Added Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Navbar.scss"
+import { images } from "../../constants";
+import { Fade as Hamburger } from 'hamburger-react'
 
 const Navbar = () => {
+
+  const [toggle, setToggle] = useState(false)
+
+  const handleClick = () => setToggle(!toggle)
+
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className="navbar__logo">
+        <img src={images.logo} alt="" />
+      </div>
+      <div className={toggle ? "navbar__links toggle" : "navbar__links"} onClick={() => setToggle(false)}>
+        <div className="nav-links">
+          <a href="/">Home</a>
+          <a href="/">About us</a>
+          <a href="/">Sapphire</a>
+          <a href="/">Contact us</a>
+          <button>Let's Talk</button>
+        </div>
+      </div>
+      <div className='nav__links' onClick={handleClick}>
+        <Hamburger size={25} duration={0.8} toggled={toggle} toggle={setToggle} />
+      </div>
+    </div>
   )
 }
 
