1d985991e7b0948c286e4c32c925b6e91c528cb2
hafizmp <hafiznjh007@gmail.com>
2023-02-14 15:42:10 +0530

    Added Footer

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     <Testimonials />
     <Enroll />
     <Contact />
-    {/* <Footer /> */}
+    <Footer />
     </>
   );
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,17 @@ import "./Footer.scss"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="copyright">
+        <h5>© 2022  milliondotsedu  |  All Rights Reserved  |  Designed by Noviindus</h5>
+      </div>
+
+      <div className="footer-links">
+        <a href="/">About us</a>
+        <a href="/">Sapphire</a>
+        <a href="/">Contact Us</a>
+      </div>
+    </footer>
   )
 }
 
